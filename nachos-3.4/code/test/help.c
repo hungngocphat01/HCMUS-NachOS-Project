0893386046b9d076d234ca59607e8c488d4a8304
NaTe <89866103+YoshidaNaTe@users.noreply.github.com>
2021-12-06 13:21:05 +0700

    Add files via upload

---
+++ nachos-3.4/code/test/help.c
@@ -0,0 +1,28 @@
+#include "syscall.h"
+/*
+    Chuong trinh in thong tin thanh vien nhom va thong tin chuong trinh sort va ascii. Co ban chi la goi PrintString de in ra console.
+*/
+int
+main()
+{
+PrintString("------------TRUONG DAI HOC KHOA HOC TU NHIEN------------\n");
+PrintString(" ------------DAI HOC QUOC GIA TP HO CHI MINH-----------\n\n");
+PrintString("         DO AN LAP TRINH SYSTEMCALL TREN NACHOS\n\n");
+PrintString("\tLop: 19_1\n");
+PrintString("\tGiang vien ly thuyet: Le Viet Long\n");
+PrintString("\tGiang vien thuc hanh: Nguyen Thanh Quan\n\n");
+PrintString("            -----THONG TIN THANH VIEN-----\n\n");
+PrintString("\t19120615 - Hung Ngoc Phat\n");
+PrintString("\t19120630 - Nguyen Van Quan\n");
+PrintString("\t19120688 - Do Nhat Toan\n");
+PrintString("\t19120729 - Bui Ngoc Thao vy\n\n");
+PrintString("            -----MO TA VE 2 CHUONG TRINH DEMO-----\n\n");
+PrintString("Chuong trinh ascii: Don gian chi la chay vong lap for de in ra 128 ky tu trong bang ma ascii\n");
+PrintString("\nChuong trinh sort: \n");
+PrintString("\t-Nhap so luong phan tu cua mang can sap xep (n<=100)\n");
+PrintString("\t-Lan luot nhap n phan tu vao trong mang\n");
+PrintString("\t-Sau khi nhap xong, chuong trinh chay thuat toan Bubble Sort de sap xep.\n");
+PrintString("\t-In ra mang da duoc sap xep tang dan.\n");
+
+PrintString("\n");
+}
