5e39a102d404acb47db028fdc258c6af38bb2f92
NaTe <89866103+YoshidaNaTe@users.noreply.github.com>
2021-12-07 10:26:52 +0700

    Update ascii.c

--- nachos-3.4/code/test/ascii.c
+++ nachos-3.4/code/test/ascii.c
@@ -1,15 +1,6 @@
 #include "syscall.h"
 int main()
 {
-//     char i = 0;
-// 	PrintString("-------BANG MA ASCII-------\n");
-// 	for (; i < 127; i++) //i <= 127
-// 	{
-// 		PrintInt((int)i);
-// 		PrintChar('\t');
-// 		PrintChar(i);
-// 		PrintChar('\n');
-// 	}
 	char i = 32;
 	PrintString("-------BANG MA ASCII-------\n"); //Chỉ in những mã ascii in được
 	for (; i < 127; i++)
